277d0f60a3592a11c068456a1fb2f31fdf413789
Akos <akospaddi54@gmail.com>
2022-03-12 13:19:24 +0100

    Initial commit

---
+++ src/Main/hf.java
@@ -0,0 +1,22 @@
+package Main;
+
+import java.util.Scanner;
+
+public class hf {
+    {
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Adj meg egy számot!");
+        int userNumber1 = scanner.nextInt();
+
+
+
+
+
+
+
+    }}
+
---
+++ src/valami/gyufak.java
@@ -0,0 +1,171 @@
+package valami;
+
+import javax.crypto.spec.PSource;
+import java.util.Scanner;
+
+public class gyufak {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+       /* System.out.println("Szabályzat");
+        System.out.println("Kérem a számot amivel játszani fogtok:");
+        int jszám = scanner.nextInt();
+        System.out.println("hány gyufát veszel el ");
+        int játékos1 = scanner.nextInt();
+        scanner.nextLine();
+
+
+
+        while (játékos1< jszám ) {
+        scanner.nextInt();
+            System.out.println("Játékos nyert");
+        }
+
+
+
+        */
+
+       /* System.out.println("kérek 5 számot");
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        int num3 = scanner.nextInt();
+        int num4 = scanner.nextInt();
+        int num5 = scanner.nextInt();
+
+        String  numsToString = num1 + ", " + num2 + ", " + num3 + ", " + num4 + ", " + num5;
+
+        int sum = num1 +num2 + num3 +num4 +num5;
+
+
+        System.out.println(" 5 szám összege"+numsToString );
+        System.out.println("Összeadva:"+ sum );
+
+        */
+
+        // TÖMB == array ? TRUE !!!!
+
+        // üres tömb
+       /* int[] numbers = new int[5];// default érték = 5 db 0
+
+        int x = numbers[0];
+        System.out.println(numbers[0]);
+
+        */
+
+       /* int[] numbers2 = new int[]{1,2,3,4,5};  // ezek már nem a deaflut érkéket tartalmazza hanem az 1-2-3-4-5.
+
+        int[] numbers3 = {6,7,8,9,10};
+        */
+
+        /* int[] numbers3 = {6 , 7 , 8 , 9 , 10};
+
+         */
+
+       /* int x = numbers3[0];
+        System.out.println("eredeti érték:" + numbers3[0]);
+        numbers3[0] =42;
+        System.out.println("Módosított érték:" +numbers3[0]);
+
+
+      /*  System.out.println(numbers3[0]);
+        System.out.println(numbers3[1]);
+        System.out.println(numbers3[2]);   ///////// így lehet kihozni a number számokat
+        System.out.println(numbers3[3]);
+        System.out.println(numbers3[4]);
+       */
+
+       /* int[] numbers4 = {6 , 7 , 8 , 9 , 10};
+
+        // int x = numbers3[0];
+        System.out.println("eredeti érték:" + numbers3[0]);
+        numbers3[3] =67;
+        System.out.println("Módosított érték:" +numbers3[3]);
+
+        numbers4[4] = 12;
+        System.out.println("Módosított érték: "+ numbers4[4]);
+
+        */
+      /*  double[] doubles = new double[2];
+        String[] strings = new String[2];                                     /// így hozol lére új tömböt
+        boolean[] booleans = new boolean[2];
+
+        System.out.println(doubles[0]);
+        System.out.println(strings[0]);
+        System.out.println(booleans[0]);
+
+       */
+      /*  int[] numbers4 = new int[5];
+        System.out.println("Kérek 5 számot");
+
+        numbers4[0] =scanner.nextInt();
+        numbers4[1] =scanner.nextInt();
+        numbers4[2] =scanner.nextInt();
+        numbers4[3] =scanner.nextInt();
+        numbers4[4] =scanner.nextInt();
+
+        int sum = numbers4[0] + numbers4[1]  + numbers4[2] + numbers4[3]+numbers4[4];
+        System.out.println("eredmény:" + sum);
+
+
+       */
+
+      /*  char[] tomb = new char[10];
+        System.out.println(tomb[9]);
+
+       */
+      /*  int[] numbers2 = {1,2,3,4,5};
+
+
+        int index=0;
+        while (index <5) {
+            System.out.println(numbers2[index]);
+            index++;
+        }
+
+        System.out.println("Kérek 5 számot");
+
+       */
+
+       /* System.out.println("hány számot akarsz megadni");
+        int szamok = scanner.nextInt();
+
+        int[] usernumbers = new int[szamok];
+
+        System.out.println("Add neg a számaidat "+ szamok+"darab szám");
+
+        int index=0;*/
+
+
+        String[] strings = {"első", "második", "harmadik", "negyedik", "ötödik", "hatodik", "hetedik"};
+        int index=0;
+        while (index < 7) {
+            System.out.println(strings[index]);
+            index +=2; }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }}
+
+
+
+
+
+
+
+
+
+
+
+
